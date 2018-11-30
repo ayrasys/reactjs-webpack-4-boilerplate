2fbe499da1cbdc90bfa8432cda1fccefaf062f9c
Ajay Kumar <ajay.nearpins@gmail.com>
2018-11-30 16:04:21 +0530

    initial commit

---
+++ src/js/components/presentational/ajay.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ajay extends Component {
+    render() {
+        return (
+            <div>
+               <form action="">
+               <input type="text"/>
+               <button>Ok</button>
+               this is niece
+               </form> 
+            </div>
+        );
+    }
+}
+
+export default ajay;
